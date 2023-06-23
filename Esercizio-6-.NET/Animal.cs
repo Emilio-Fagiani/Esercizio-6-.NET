c5f52aeda6ee2584504a60098d66750421bdbbae
emifa <emifa@LAPTOP-1TM449QB>
2023-06-23 12:02:16 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-6-.NET/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_6_.NET
+{
+   
+        public class Animal
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+
+            public virtual void Speak()
+            {
+                Console.WriteLine("Make a sound");
+            }
+        }
+    }
+
---
+++ Esercizio-6-.NET/Program.cs
@@ -0,0 +1,7 @@
+﻿using Esercizio_6_.NET;
+
+Dog dog = new Dog {name = "Bob", age = 1 };
+Cat cat = new Cat { name = "Luna" ,age = 1 };
+
+cat.Speak();
+dog.Speak();    
